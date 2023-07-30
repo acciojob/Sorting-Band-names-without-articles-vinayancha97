663e00f98f1b6516649a712758f3f7036ab33f1f
vinayancha97 <135926111+vinayancha97@users.noreply.github.com>
2023-07-30 10:14:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 //your code here
+const bands=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+ function sortBandNames(bandNames) {
+    // Regular expression to match articles ('a', 'an', 'the') at the beginning of the band names
+    const articleRegex = /^(a|an|the)\s/i;
+
+    return bandNames.sort((a, b) => {
+      // Remove articles from band names for comparison
+      const bandNameA = a.replace(articleRegex, '');
+      const bandNameB = b.replace(articleRegex, '');
+
+      // Compare the band names without articles in lexicographic order
+      return bandNameA.localeCompare(bandNameB);
+    });
+  }
+
+  function displayBandNamesInUL(bandNames) {
+    const sortedBandNames = sortBandNames(bandNames);
+    const ulElement = document.getElementById("band");
+
+    // Clear existing contents of the ul element
+    ulElement.innerHTML = '';
+
+    // Generate and append li elements for each band name
+    sortedBandNames.forEach((bandName) => {
+      const liElement = document.createElement("li");
+      liElement.textContent = bandName;
+      ulElement.appendChild(liElement);
+    });
+  }
+
+  // Call the function to display the sorted band names inside the ul element
+  displayBandNamesInUL(bandNames);
